badda2978ef1e992bb40a5759d711c86cbe195c0
Cole Barach <colebarach@tutanota.com>
2025-10-09 11:07:00 -0400

    More mdf-dump refactoring. Data section parsing is now moved into a generic method, with a callback for handling the data byte-by-byte.

--- src/mdf_dump/main.c
+++ src/mdf_dump/main.c
@@ -56,6 +56,8 @@ typedef struct
 	uint8_t* dataSection;
 } mdfBlock_t;
 
+typedef void (mdfByteHandler_t) (mdfBlock_t* block, uint8_t data, void* arg);
+
 // Functions ------------------------------------------------------------------------------------------------------------------
 
 int handleFreadError (FILE* stream)
@@ -102,7 +104,7 @@ FILE* openMdf (const char* filePath, mdfFileIdBlock_t* fileIdBlock)
 	return stream;
 }
 
-int readBlock (FILE* stream, mdfBlock_t* block)
+int readBlockHeader (FILE* stream, mdfBlock_t* block)
 {
 	// Store the address of the block
 	block->addr = ftell (stream);
@@ -111,16 +113,43 @@ int readBlock (FILE* stream, mdfBlock_t* block)
 	if (fread (&block->header, sizeof (block->header), 1, stream) != 1)
 		return handleFreadError (stream);
 
+	return 0;
+}
+
+int readBlockLinkList (FILE* stream, mdfBlock_t* block)
+{
 	// Read the block's link list
 	block->linkList = malloc (block->header.linkCount * sizeof (uint64_t));
+	if (block->linkList == NULL)
+		return errno;
+
 	if (fread (block->linkList, sizeof (uint64_t), block->header.linkCount, stream) != block->header.linkCount)
 		return handleFreadError (stream);
 
-	block->dataSectionSize = block->header.blockLength - sizeof (block->header) - sizeof (uint64_t) * block->header.linkCount;
+	return 0;
+}
 
-	// TODO(Barach): Figure this out.
-	// // Read the block's data section
-	// block->dataSection = malloc (block->dataSectionSize);
+int readBlockDataSection (FILE* stream, mdfBlock_t* block, mdfByteHandler_t* dataSectionByteHandler, void* dataSectionByteArg)
+{
+	// Read the block's data section. Note that a data section size of zero indicates the block's data section extends to the
+	// end of the file.
+	block->dataSectionSize = block->header.blockLength - sizeof (block->header) - sizeof (uint64_t) * block->header.linkCount;
+	for (size_t index = 0; index < block->dataSectionSize || block->dataSectionSize == 0; ++index)
+	{
+		int data = fgetc (stream);
+		if (data == EOF)
+		{
+			// If we expected more data, return error.
+			if (block->dataSectionSize != 0)
+				return handleFreadError (stream);
+
+			break;
+		}
+
+		// Pass the received byte to the user provided handler.
+		dataSectionByteHandler (block, (uint8_t) data, dataSectionByteArg);
+	}
+
 	return 0;
 }
 
@@ -184,6 +213,193 @@ void dumpBlockLinkList (FILE* stream, mdfBlock_t* block)
 	}
 }
 
+typedef struct
+{
+	FILE* timestampStream;
+
+	size_t recordIndex;
+	uint8_t recordId;
+
+	uint8_t dlc0;
+	uint8_t dlc1;
+	uint8_t dlc2;
+
+	uint64_t timestamp0;
+	uint64_t timestamp1;
+	uint32_t canId;
+} dataSectionArg_t;
+
+void dataSectionByteHandler (mdfBlock_t* block, uint8_t data, void* dataSectionByteArg)
+{
+	dataSectionArg_t* arg = dataSectionByteArg;
+
+	switch (block->header.blockId)
+	{
+	case BLOCK_ID_DT:
+		printf ("%02X ", data);
+
+		// Record ID
+		if (arg->recordIndex == 0)
+		{
+			arg->recordId = data;
+			switch (arg->recordId)
+			{
+			case 1:
+				printf ("(CAN RX)  | ");
+				break;
+
+			default:
+				printf ("(Unknown) | ");
+				fprintf (stderr, "Warning: Unknown record ID '0x%04X'.\n", arg->recordId);
+				break;
+			}
+		}
+
+		switch (arg->recordId)
+		{
+		case 1:
+			// Time stamp 0
+
+			if (arg->recordIndex == 2)
+				arg->timestamp0 = 0;
+			if (2 <= arg->recordIndex && arg->recordIndex < 9)
+			{
+				arg->timestamp0 |= ((uint64_t) data) << ((arg->recordIndex - 2) * 8);
+			}
+			if (arg->recordIndex == 8)
+			{
+				fprintf (arg->timestampStream, "0x%016lX, %lu, ", arg->timestamp0, arg->timestamp0);
+				printf ("(Timestamp 0 = %16lu) | ", arg->timestamp0);
+			}
+
+			// CAN ID
+
+			if (arg->recordIndex == 9)
+				arg->canId = 0;
+			if (9 <= arg->recordIndex && arg->recordIndex < 13)
+				arg->canId |= data << ((arg->recordIndex - 9) * 8);
+			if (arg->recordIndex == 12)
+			{
+				arg->canId >>= 3;
+				printf ("(CAN ID = 0x%03X) | ", arg->canId);
+			}
+
+			// DLC 0
+
+			if (arg->recordIndex == 13)
+			{
+				if (data % 2 != 0)
+					fprintf (stderr, "Warning: Fractional DLC 0.\n");
+
+				if (data / 2 <= 8)
+				{
+					arg->dlc0 = data / 2;
+					printf ("(DLC 0 = %u) | ", arg->dlc0);
+				}
+				else
+				{
+					arg->dlc0 = 0;
+					printf ("(DLC 0 invalid) | ");
+					fprintf (stderr, "Warning: Invalid DLC 0 '%u'.\n", data);
+				}
+			}
+
+			if (arg->recordIndex == 14)
+			{
+				if (arg->dlc0 * 16 != data)
+					fprintf (stderr, "Warning: Mismatched DLC 1, '%u' != '%u'.\n", arg->dlc0 * 16, data);
+
+				if (data / 16 <= 8)
+				{
+					arg->dlc1 = data / 16;
+					printf ("(DLC 1 = %u) | ", arg->dlc1);
+				}
+				else
+				{
+					arg->dlc1 = 0;
+					printf ("(DLC 1 invalid) | ");
+					fprintf (stderr, "Warning: Invalid DLC 1 '%u'.\n", data);
+				}
+			}
+
+			// Timestamp 1
+			if (arg->recordIndex == 14)
+				arg->timestamp1 = 0;
+			if (14 <= arg->recordIndex && arg->recordIndex < 18)
+			{
+				arg->timestamp1 |= ((uint64_t) data) << ((arg->recordIndex - 14) * 8);
+			}
+			if (arg->recordIndex == 17)
+			{
+				fprintf (arg->timestampStream, "0x%08lX, %lu, \n", arg->timestamp1, arg->timestamp1);
+			}
+
+			// DLC
+
+			if (arg->recordIndex == 23)
+				printf ("| ");
+
+			if (arg->recordIndex == 24)
+			{
+				if (data != arg->dlc0)
+					fprintf (stderr, "Warning: Mismatched DLC 2, '%u' != '%u'.\n", data, arg->dlc0);
+
+				if (data <= 8)
+				{
+					arg->dlc2 = data;
+					printf ("(DLC 2 = %u) | ", arg->dlc2);
+				}
+				else
+				{
+					arg->dlc2 = 0;
+					printf ("(DLC invalid) | ");
+					fprintf (stderr, "Warning: Invalid DLC 2 '%u'.\n", data);
+				}
+			}
+
+			// Payload
+
+			if (arg->recordIndex == 27)
+				printf ("| ");
+
+			if (arg->recordIndex == 27U + arg->dlc0)
+			{
+				arg->recordIndex = 0;
+				if (arg->dlc0 == 0)
+					printf ("(No Payload)\n");
+				else
+					printf ("(Payload)\n");
+				return;
+			}
+			break;
+
+		default:
+			if (arg->recordIndex == 32)
+			{
+				printf ("\n");
+				arg->recordIndex = 0;
+				return;
+			}
+		}
+	break;
+
+	case BLOCK_ID_MD:
+	case BLOCK_ID_TX:
+		if (data != '\0')
+			printf ("%c", data);
+		break;
+
+	default:
+		printf ("%02X ", data);
+
+		if (arg->recordIndex % 32 == 31)
+			printf ("\n");
+		break;
+	}
+
+	++arg->recordIndex;
+}
+
 int main (int argc, char** argv)
 {
 	debugInit ();
@@ -239,16 +455,23 @@ int main (int argc, char** argv)
 			break;
 
 		mdfBlock_t block;
-		if (readBlock (mdf, &block) != 0)
+		if (readBlockHeader (mdf, &block) != 0)
 		{
 			int code = errno;
-			fprintf (stderr, "Failed to read MDF block: %s.\n", errorMessage (code));
+			fprintf (stderr, "Failed to read MDF block header: %s.\n", errorMessage (code));
 			return code;
 		}
 
 		printf ("- Begin Header - 0x%08lX -\n\n", block.addr);
 		dumpBlockHeader (stdout, &block);
 		printf ("- End Header -\n\n");
+
+		if (readBlockLinkList (mdf, &block) != 0)
+		{
+			int code = errno;
+			fprintf (stderr, "Failed to read MDF block link list: %s.\n", errorMessage (code));
+			return code;
+		}
 
 		size_t linkLinkLength = 8 * block.header.linkCount;
 		if (linkLinkLength != 0)
@@ -260,204 +483,17 @@ int main (int argc, char** argv)
 
 		printf ("- Begin Data Section -\n\n");
 
-		// TODO(Barach): Parse this from the MDF file?
-		enum : uint8_t
+		dataSectionArg_t dataSectionByteArg =
 		{
-			ID_CAN_RX	= 0x01
-		} recordId = 0;
+			.timestampStream = timestampStream,
+			.recordIndex = 0,
+		};
 
-		uint8_t dlc0;
-		uint8_t dlc1;
-		uint8_t dlc2;
-
-		size_t recordIndex = 0;
-		size_t dataCount = 0;
-		uint64_t timestamp0 = 0;
-		uint64_t timestamp1 = 0;
-		uint32_t canId = 0;
-
-		while (dataCount < block.dataSectionSize || block.dataSectionSize == 0)
+		if (readBlockDataSection (mdf, &block, dataSectionByteHandler, &dataSectionByteArg) != 0)
 		{
-			int data = fgetc (mdf);
-			if (data == EOF)
-			{
-				if (block.dataSectionSize != 0)
-				{
-					int code = handleFreadError (mdf);
-					fprintf (stderr, "Failed to read data section: %s.\n", errorMessage (code));
-					return code;
-				}
-
-				break;
-			}
-			++dataCount;
-
-			if (block.header.blockId == BLOCK_ID_DT)
-			{
-				printf ("%02X ", data);
-
-				// Record ID
-				if (recordIndex == 0)
-					recordId = 0;
-
-				if (recordIndex < sizeof (recordId))
-					recordId |= data << (recordIndex * 8);
-
-				if (recordIndex == sizeof (recordId) - 1)
-				{
-					switch (recordId)
-					{
-					case ID_CAN_RX:
-						printf ("(CAN RX)  | ");
-						break;
-
-					default:
-						printf ("(Unknown) | ");
-						fprintf (stderr, "Warning: Unknown record ID '0x%04X'.\n", recordId);
-						break;
-					}
-				}
-
-				if (recordId == ID_CAN_RX)
-				{
-					// Time stamp 0
-
-					if (recordIndex == 2)
-						timestamp0 = 0;
-					if (2 <= recordIndex && recordIndex < 9)
-					{
-						timestamp0 |= ((uint64_t) data) << ((recordIndex - 2) * 8);
-					}
-					if (recordIndex == 8)
-					{
-						fprintf (timestampStream, "0x%016lX, %lu, ", timestamp0, timestamp0);
-						printf ("(Timestamp 0 = %16lu) | ", timestamp0);
-					}
-
-					// CAN ID
-
-					if (recordIndex == 9)
-						canId = 0;
-					if (9 <= recordIndex && recordIndex < 13)
-						canId |= data << ((recordIndex - 9) * 8);
-					if (recordIndex == 12)
-					{
-						canId >>= 3;
-						printf ("(CAN ID = 0x%03X) | ", canId);
-					}
-
-					// DLC 0
-
-					if (recordIndex == 13)
-					{
-						if (data % 2 != 0)
-							fprintf (stderr, "Warning: Fractional DLC 0.\n");
-
-						if (data / 2 >= 0 && data / 2 <= 8)
-						{
-							dlc0 = data / 2;
-							printf ("(DLC 0 = %u) | ", dlc0);
-						}
-						else
-						{
-							dlc0 = 0;
-							printf ("(DLC 0 invalid) | ");
-							fprintf (stderr, "Warning: Invalid DLC 0 '%u'.\n", data);
-						}
-					}
-
-					if (recordIndex == 14)
-					{
-						if (dlc0 * 16 != data)
-							fprintf (stderr, "Warning: Mismatched DLC 1, '%u' != '%u'.\n", dlc0 * 16, data);
-
-						if (data / 16 >= 0 && data / 16 <= 8)
-						{
-							dlc1 = data / 16;
-							printf ("(DLC 1 = %u) | ", dlc1);
-						}
-						else
-						{
-							dlc1 = 0;
-							printf ("(DLC 1 invalid) | ");
-							fprintf (stderr, "Warning: Invalid DLC 1 '%u'.\n", data);
-						}
-					}
-
-					// Timestamp 1
-					if (recordIndex == 14)
-						timestamp1 = 0;
-					if (14 <= recordIndex && recordIndex < 18)
-					{
-						timestamp1 |= ((uint64_t) data) << ((recordIndex - 14) * 8);
-					}
-					if (recordIndex == 17)
-					{
-						fprintf (timestampStream, "0x%08lX, %lu, \n", timestamp1, timestamp1);
-					}
-
-					// DLC
-
-					if (recordIndex == 23)
-						printf ("| ");
-
-					if (recordIndex == 24)
-					{
-						if (data != dlc0)
-							fprintf (stderr, "Warning: Mismatched DLC 2, '%u' != '%u'.\n", data, dlc0);
-
-						if (data >= 0 && data <= 8)
-						{
-							dlc2 = data;
-							printf ("(DLC 2 = %u) | ", dlc2);
-						}
-						else
-						{
-							dlc2 = 0;
-							printf ("(DLC invalid) | ");
-							fprintf (stderr, "Warning: Invalid DLC 2 '%u'.\n", data);
-						}
-					}
-
-					// Payload
-
-					if (recordIndex == 27)
-						printf ("| ");
-
-					if (recordIndex == 27U + dlc0)
-					{
-						recordIndex = 0;
-						if (dlc0 == 0)
-							printf ("(No Payload)\n");
-						else
-							printf ("(Payload)\n");
-						continue;
-					}
-				}
-				else
-				{
-					if (recordIndex == 32)
-					{
-						printf ("\n");
-						recordIndex = 0;
-						continue;
-					}
-				}
-
-				++recordIndex;
-			}
-			else if (block.header.blockId == BLOCK_ID_MD || block.header.blockId == BLOCK_ID_TX)
-			{
-				if (data != '\0')
-					printf ("%c", data);
-			}
-			else
-			{
-				printf ("%02X ", data);
-
-				if (dataCount % 32 == 0)
-					printf ("\n");
-			}
+			int code = errno;
+			fprintf (stderr, "Failed to read MDF block data section: %s.\n", errorMessage (code));
+			return code;
 		}
 
 		printf ("\n\n- End Data Section -\n\n");
