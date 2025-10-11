0b7db78d54215dc8d5115135c876c85eb09ef50d
Cole Barach <colebarach@tutanota.com>
2025-10-11 17:09:36 -0400

    data-logger can now write to files without using shell redirection.

--- src/data_logger/main.c
+++ src/data_logger/main.c
@@ -12,8 +12,26 @@ int main (int argc, char** argv)
 
 	if (argc < 2)
 	{
-		fprintf (stderr, "Invalid arguments, usage: data-logger <CAN device>.\n");
+		fprintf (stderr, "Invalid arguments, usage: data-logger <options> <CAN device>.\n");
 		return -1;
+	}
+
+	FILE* file = stdout;
+	for (int index = 1; index < argc - 1; ++index)
+	{
+		if (argv [index][0] == '-' && argv [index][1] == 'f' && argv [index][2] == '=')
+		{
+			file = fopen (argv [index] + 3, "w");
+			if (file == NULL)
+			{
+				int code = errno;
+				fprintf (stderr, "Failed to open MDF file: %s.\n", errorMessage (code));
+				return code;
+			}
+			continue;
+		}
+
+		fprintf (stderr, "Warning: Unrecognized option '%s'.\n", argv [index]);
 	}
 
 	char* deviceName = argv [argc - 1];
@@ -34,10 +52,10 @@ int main (int argc, char** argv)
 			continue;
 		}
 
-		printf ("0x%03X", frame.id);
+		fprintf (file, "0x%03X", frame.id);
 		for (size_t index = 0; index < frame.dlc; ++index)
-			printf ("[0x%02X]", frame.data [index]);
-		printf ("\n");
+			fprintf (file, "[0x%02X]", frame.data [index]);
+		fprintf (file, "\n");
 	}
 
 	return 0;
