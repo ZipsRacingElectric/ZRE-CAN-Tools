7d091f6adef89c308a49b9592cda138aba564ead
Cole Barach <colebarach@tutanota.com>
2026-02-18 14:21:07 -0500

    Fixed the ZRE24 shell scripts not working on Windows.

--- src/shell/cross-bms.bat
+++ src/shell/cross-bms.bat
@@ -4,19 +4,14 @@ setlocal enabledelayedexpansion
 :: Arguments:
 :: - 1 - Device name (optional)
 
-:: Initialize the CAN device
-for /f %%i in ('"%ZRE_CANTOOLS_DIR%/bin/can-init" 1000000 %~1') do set "DEVICE=%%i"
-if [!DEVICE!]==[] (
-	echo Press enter to close...
-	pause >nul
-	exit /b -1
-)
-
 :: Set minimum terminal width
 mode 133, 54
 
-:: Start the application
-"%ZRE_CANTOOLS_DIR%/bin/bms-tui.exe" !DEVICE! "%ZRE_CANTOOLS_DIR%/config/zre24_cross/can.dbc" "%ZRE_CANTOOLS_DIR%/config/zre24_cross/bms_config.json"
+IF [%1] == [] (
+	"%ZRE_CANTOOLS_DIR%/bin/bms-tui.exe" "COM*@500000" "%ZRE_CANTOOLS_DIR%/config/zre24_cross/can.dbc" "%ZRE_CANTOOLS_DIR%/config/zre24_cross/bms_config.json"
+) ELSE (
+	"%ZRE_CANTOOLS_DIR%/bin/bms-tui.exe" "%1@500000" "%ZRE_CANTOOLS_DIR%/config/zre24_cross/can.dbc" "%ZRE_CANTOOLS_DIR%/config/zre24_cross/bms_config.json"
+)
 
 echo Press enter to close...
 pause >nul
