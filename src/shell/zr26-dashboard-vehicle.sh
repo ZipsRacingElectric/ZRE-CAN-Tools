fcc0c53ee2c6126ca9d71e8b240f231db2651ab3
Cole Barach <colebarach@tutanota.com>
2026-03-27 13:41:30 -0400

    Finally split ZR26's DBC file into two separate ones: main.dbc and sensor.dbc

--- src/shell/zr26-dashboard-vehicle.sh
+++ src/shell/zr26-dashboard-vehicle.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Arguments:
-# - 1 - Device name (optional)
+# - 1 - Device 0 name (optional)
 
 if [ "$1" == "" ]; then
-	$ZRE_CANTOOLS_DIR/bin/dashboard-gui $ZRE_CANTOOLS_DIR/config/zr26/vehicle/dashboard_gui.json "/dev/tty*@1000000"
+	$ZRE_CANTOOLS_DIR/bin/dashboard-gui $ZRE_CANTOOLS_DIR/config/zr26/vehicle/dashboard_gui.json "/dev/tty*@1000000" "/dev/tty*@1000000"
 else
-	$ZRE_CANTOOLS_DIR/bin/dashboard-gui $ZRE_CANTOOLS_DIR/config/zr26/vehicle/dashboard_gui.json "$1@1000000"
+	$ZRE_CANTOOLS_DIR/bin/dashboard-gui $ZRE_CANTOOLS_DIR/config/zr26/vehicle/dashboard_gui.json "$1@1000000" "/dev/tty*@1000000"
 fi
