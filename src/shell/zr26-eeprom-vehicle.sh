6e3e2f3bdedcd6ccaa3ebad5b67d74d7fb1465dc
Cole Barach <colebarach@tutanota.com>
2026-07-12 12:04:43 -0400

    Added MSB to the list of configs for ZR26.

--- src/shell/zr26-eeprom-vehicle.sh
+++ src/shell/zr26-eeprom-vehicle.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$ZRE_CANTOOLS_DIR/bin/can-eeprom-cli "$@" "/dev/tty*@1000000" $ZRE_CANTOOLS_DIR/config/zr26/vcu_config.json $ZRE_CANTOOLS_DIR/config/zr26/bms_config.json $ZRE_CANTOOLS_DIR/config/zr26/drs_config.json
+$ZRE_CANTOOLS_DIR/bin/can-eeprom-cli "$@" "/dev/tty*@1000000" $ZRE_CANTOOLS_DIR/config/zr26/vcu_config.json $ZRE_CANTOOLS_DIR/config/zr26/bms_config.json $ZRE_CANTOOLS_DIR/config/zr26/msb_rear_config.json
